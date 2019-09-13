84a9a4f733c43f1af23baf89e5d0499a507a7add
mahak <makk.bit@outlook.com>
2019-09-13 07:38:54 +0530

    Updates

---
+++ precache-manifest.bb7220e7e23d45ce0140a12bcd5d00a3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "afa398c21d44f357a663",
+    "url": "/makkBit/aao-chat-kare/static/css/main.24c6359a.chunk.css"
+  },
+  {
+    "revision": "bd53456ba20426464180",
+    "url": "/makkBit/aao-chat-kare/static/css/0.f908102a.chunk.css"
+  },
+  {
+    "revision": "c11efa6c8057956c929b",
+    "url": "/makkBit/aao-chat-kare/static/css/home.823768c8.chunk.css"
+  },
+  {
+    "revision": "c11efa6c8057956c929b",
+    "url": "/makkBit/aao-chat-kare/static/js/home.1ddf467e.chunk.js"
+  },
+  {
+    "revision": "4ab459211279ed6d4315",
+    "url": "/makkBit/aao-chat-kare/static/css/login.e798d74a.chunk.css"
+  },
+  {
+    "revision": "4ab459211279ed6d4315",
+    "url": "/makkBit/aao-chat-kare/static/js/login.ccbb8cec.chunk.js"
+  },
+  {
+    "revision": "bd53456ba20426464180",
+    "url": "/makkBit/aao-chat-kare/static/js/0.533aad1e.chunk.js"
+  },
+  {
+    "revision": "afa398c21d44f357a663",
+    "url": "/makkBit/aao-chat-kare/static/js/main.5973c37b.chunk.js"
+  },
+  {
+    "revision": "d3fd9a8fccf92b25b3b8",
+    "url": "/makkBit/aao-chat-kare/static/js/runtime~main.30b3c9fb.js"
+  },
+  {
+    "revision": "527833f27db0b598b134",
+    "url": "/makkBit/aao-chat-kare/static/js/5.85427568.chunk.js"
+  },
+  {
+    "revision": "14db0c66ae08b4c1e6f0",
+    "url": "/makkBit/aao-chat-kare/static/css/6.d2fd2426.chunk.css"
+  },
+  {
+    "revision": "14db0c66ae08b4c1e6f0",
+    "url": "/makkBit/aao-chat-kare/static/js/6.3e5c5d50.chunk.js"
+  },
+  {
+    "revision": "27c222dcb838dd5f49e8fc2a6d0c8406",
+    "url": "/makkBit/aao-chat-kare/index.html"
+  }
+];
